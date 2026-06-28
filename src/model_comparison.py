854c11c8c412c621b765eae86bd2224ee0a10245
Deepakkr-99 <dk719111@gmail.com>
2026-06-28 22:23:14 +0530

    Delete src directory

--- src/model_comparison.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# model training ke baad bani hui csv read karo
-df = pd.read_csv("data/model_result.csv")
-
-print("===== Model Comparison =====")
-print(df)
-
-print("\nBest Model:", df.loc[df["Accuracy"].idxmax(), "Model"])
